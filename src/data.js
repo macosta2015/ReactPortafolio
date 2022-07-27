4a5c2a02ce1fcf574a2577cc375be64cc6311c61
macosta2015 <macosta2015@my.fit.edu>
2022-07-27 19:34:28 -0400

    Adding the photos

--- src/data.js
+++ src/data.js
@@ -2,31 +2,31 @@ export const products = [
   {
     id: 1,
     img: "https://upload.wikimedia.org/wikipedia/en/7/7f/Florida_Institute_of_Technology_%28seal%29.svg",
-    link: "https://angelmond.github.io/Crypto-Check/?selectCrypto=+",
+    link: "https://www.linkedin.com/in/mario-acosta-b4337512a/",
   },
   {
     id: 2,
-    img: "../components/portafolioPhoto/CryptoCheck.jpg",
+    img: "https://raw.githubusercontent.com/macosta2015/UpdatedPortafolio/main/images/WorkDayScheduler.jpg",
     link: "https://macosta2015.github.io/FullStackHW6/",
   },
   {
     id: 3,
-    img: "components/portafolioPhoto/CryptoCheck.jpg",
-    link: "https://www.linkedin.com/feed/",
+    img: "https://raw.githubusercontent.com/macosta2015/UpdatedPortafolio/main/images/CryptoCheck.jpg",
+    link: "https://raw.githubusercontent.com/macosta2015/UpdatedPortafolio/main/images/CryptoCheck.jpg",
   },
   {
     id: 4,
-    img: "https://macosta2015.github.io/TEST-REPOSITORY-FOR-WEB/",
+    img: "https://raw.githubusercontent.com/macosta2015/UpdatedPortafolio/main/images/PasswordGenerator.jpg",
     link: "https://macosta2015.github.io/TEST-REPOSITORY-FOR-WEB/",
   },
   {
     id: 5,
-    img: "https://pin-it-app-project.herokuapp.com/",
+    img: "https://raw.githubusercontent.com/macosta2015/UpdatedPortafolio/main/images/PintIt.jpg",
     link: "https://pin-it-app-project.herokuapp.com/",
   },
   {
     id: 6,
-    img: "https://gist.github.com/macosta2015/e2a85000e1bc381becb0a5765ecb51bc",
+    img: "https://raw.githubusercontent.com/macosta2015/UpdatedPortafolio/main/images/Regextutorial.jpg",
     link: "https://gist.github.com/macosta2015/e2a85000e1bc381becb0a5765ecb51bc",
   },
   {

cae10bfea79d5d815481f61b4f0a48c542db0b40
macosta2015 <macosta2015@my.fit.edu>
2022-07-27 08:39:06 -0400

    Commit the photo changes

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,26 +1,27 @@
 import "./intro.css";
-import Me from "../../img/me.png";
+import Me from "../../img/MITPhoto.png";
 
 const Intro = () => {
   return (
     <div className="i">
       <div className="i-left">
         <div className="i-left-wrapper">
-          <h2 className="i-intro">Hello, My name is</h2>
-          <h1 className="i-name">John Burton</h1>
+          <h1 className="i-name">Mario Acosta</h1>
+          <h2 className="i-intro">Software and Mechanical Engineer</h2>
           <div className="i-title">
             <div className="i-title-wrapper">
               <div className="i-title-item">Web Developer</div>
-              <div className="i-title-item">UI/UX Designer</div>
-              <div className="i-title-item">Photographer</div>
-              <div className="i-title-item">Writer</div>
-              <div className="i-title-item">Content Creator</div>
+              <div className="i-title-item">CS Minor</div>
+              <div className="i-title-item">YouTuber</div>
+              <div className="i-title-item">Reader</div>
+              <div className="i-title-item">Mechanical Engineer</div>
+              <div className="i-title-item">Data Analytics Bootcamp</div>
+              <div className="i-title-item">Full Stack Bootcamp</div>
             </div>
           </div>
           <p className="i-desc">
-            I design and develop services for customers of all sizes,
-            specializing in creating stylish, modern websites, web services and
-            online stores.
+            I am a passionate Engineer with a love for Mechanical and Software problems. 
+            Trully believe we can solve the world problems by the use of Technology.
           </p>
         </div>
         <svg

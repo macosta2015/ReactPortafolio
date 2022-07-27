8d72c71c9e74847203972c05104fd08b7248e9d9
macosta2015 <macosta2015@my.fit.edu>
2022-07-27 09:05:12 -0400

    Adding the photos into the portafolio

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -15,16 +15,13 @@ const About = () => {
         </div>
       </div>
       <div className="a-right">
-        <h1 className="a-title">About Me</h1>
+        <h1 className="a-title">Some highlits</h1>
         <p className="a-sub">
-          It is a long established fact that a reader will be distracted by the
-          readable content.
+          The best way to learn something is by doing it.
         </p>
         <p className="a-desc">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
+          I currently work as a process engineer in Miami, Fl. After work I 3D print a lot and 
+          code. The biggest world problems are able to be solved by engineering
         </p>
         <div className="a-award">
           <img src={Award} alt="" className="a-award-img" />

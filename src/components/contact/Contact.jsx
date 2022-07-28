e8bf92e2b03e981462d0086ef55b77c38b422560
macosta2015 <macosta2015@my.fit.edu>
2022-07-28 09:08:35 -0400

    Adding the URLs and needed icons on the page.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,6 +5,7 @@ import Address from "../../img/address.png";
 import { useContext, useRef, useState } from "react";
 import { ThemeContext } from "../../context";
 
+
 const Contact = () => {
   const formRef = useRef();
   const [done, setDone] = useState(false)
@@ -37,8 +38,26 @@ const Contact = () => {
         <div className="c-right">
           <p className="c-desc">
             <b>Ready to work on the next big thing!</b> 
-            <br></br>Get in touch. Always available for
+            <br>
+            </br>
+            Get in touch. Always available for
             freelancing if the right project comes along. me.
+            
+            
+            {/* <a href="https://github.com/macosta2015" class="w3-xxxlarge"><i class="fa fa-home"></i> Home</a> */}
+
+            <br></br>
+
+            <a href="https://github.com/macosta2015" class="w3-xxxlarge"><i class="fa fa-github"></i> Github</a>
+            <br></br>
+            <a href="https://twitter.com/MarioAc57592312" class="w3-xxxlarge"><i class="fa fa-twitter"></i> Twitter</a>
+            <br></br>
+            <a href="https://www.linkedin.com/in/mario-acosta-b4337512a/" class="w3-xxxlarge"><i class="fa fa-linkedin"></i> Linkedin</a>
+            <br></br>
+            <a href="https://www.youtube.com/channel/UCizQh6fUOAZJ6O_OQ7K9LqQ" class="w3-xxxlarge"><i class="fa fa-youtube"></i> YouTube</a>
+  
+
+            <br></br>
           </p>
           {/* <form ref={formRef} onSubmit={handleSubmit}>
             <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Name" name="user_name" />
